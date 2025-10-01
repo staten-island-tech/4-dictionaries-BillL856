f40c0e06e43a65ea1eae49cda5d15946e8424388
BillL856 <billl14@nycstudents.net>
2025-10-01 09:28:33 -0400

    do purchase thing next

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+applebag= {
+    "name":"Apples",
+    "price": 4.59,
+    "descriptiion":"Fresh Apples Sourced Locally Organically"
+}
+pearbag= {
+    "name":"Pears",
+    "price": 4.59,
+    "descriptiion":"Fresh Pears Sourced Locally Organically"
+}
+orangebag= {
+    "name":"Oranges",
+    "price": 4.99,
+    "descriptiion":"Fresh Oranges Sourced Locally Organically"
+}

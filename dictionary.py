e1c29bf356958153f891540b4d2d4957d12e30b2
BillL856 <billl14@nycstudents.net>
2025-10-03 09:11:35 -0400

    do part 2

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,27 @@
-applebag= {
+#part 1
+fruit_store = [
+{
     "name":"Apples",
     "price": 4.59,
     "descriptiion":"Fresh Apples Sourced Locally Organically"
-}
-pearbag= {
+},
+{
     "name":"Pears",
     "price": 4.59,
     "descriptiion":"Fresh Pears Sourced Locally Organically"
-}
-orangebag= {
+},
+{
     "name":"Oranges",
     "price": 4.99,
     "descriptiion":"Fresh Oranges Sourced Locally Organically"
 }
+]
+
+for index, item in enumerate(fruit_store):
+    print(index, ":", item["name"])
+purchase = int(input("Chose a product in the fruit store by typing an integer between 0 and 2"))
+print(fruit_store[purchase]["name"])
+
+#part 2
+
+cart= []

5652dd2e4fa371079dbab2cfb1cd232f78d2c489
BillL856 <billl14@nycstudents.net>
2025-10-14 09:15:28 -0400

    i bombed

--- practice.py
+++ practice.py
@@ -19,3 +19,29 @@ def spaces(n,y,t):
             both+=1
     return both
 print(spaces(5, "CCC..", "C.C.C"))
+
+def MC(n,s,a):
+    correct=0
+    for i in range(n):
+        if s[i]==a[i]:
+            correct+=1
+
+""" def pw(x):
+    upper=0
+    lower=0
+    digits=0
+    if len(x)>6 and len(X)<12:
+        for char in x:
+            if char.isdigit():
+                lower+=1
+        if upper>3 and lower>2 and digits>1:
+ """
+            
+def wizard(o,n,duels):
+    owner=o
+    number_of_owners=1
+    for i in range(n):
+        if duels[1][i]:
+            if duels[1]==owner:
+                owner=duels[0]
+                number_of_owners+=1
